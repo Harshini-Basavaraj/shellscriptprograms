375d8cf4198cc83eeb3f9f91764f5d9ddedac969
Harshini-Basavaraj <harshini83@gmail.com>
2021-08-20 21:32:43 +0530

    Added:Adding daily wage

--- empWgaeComputation.sh
+++ empWgaeComputation.sh
@@ -1,9 +1,18 @@
 Present=1
 Absent=0
-randomnumber=$((RANDOM%2))
+Parttime=2
+wageperhr=20
+randomnumber=$((RANDOM%3))
 if [ $Present -eq $randomnumber ]
 then
- echo "employee is Present"
+ echo "employee is Present for fulltime"
+   emp=8
+elif [ $Parttime -eq $randomnumber ]
+then
+ echo "employee is Present for Parttime"
+   emp=4
 else
- echo "employee is Absent"
+ echo "employee is absent"
 fi
+dailywage=$((wageperhr*emp))
+echo daily wage is $dailywage
